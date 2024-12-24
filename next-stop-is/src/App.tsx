5cee9ca22cd389e4c5a49a22066a34389d937960
George Fang <57463760+georgef7@users.noreply.github.com>
2024-12-24 10:54:12 -0500

    Update App.tsx with indentation

--- next-stop-is/src/App.tsx
+++ next-stop-is/src/App.tsx
@@ -6,33 +6,38 @@ import Button from '@mui/material/Button'
 import { Box } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React: Next Stop is</h1>
-      <Box>A passenger information display project</Box>
-      <div className="card">
-        <Button variant='contained' onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <Box data-testid='George-Message'>
-          Hello World! It's George! Meow~~
-        </Box>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React: Next Stop is</h1>
+            <Box>A passenger information display project</Box>
+            <div className="card">
+                <Button variant='contained' onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </Button>
+                <Box data-testid='George-Message'>
+                    Hello World! It's George! Meow~~
+                </Box>
+                <Box>
+                    {` /\_/\ \n
+( o.o ) \n
+ > ^ <`}
+                </Box>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    )
 }
 
 export default App
