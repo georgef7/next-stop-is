0d1087bb5391096d2c24aeb743eb3dc11e6c9fef
George Fang <57463760+georgef7@users.noreply.github.com>
2024-12-24 19:24:00 -0500

    Use unused variables

--- next-stop-is/cypress.config.ts
+++ next-stop-is/cypress.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setupNodeEvents(on, config) {
+      console.log(on, config);
       // implement node event listeners here
     },
   },
