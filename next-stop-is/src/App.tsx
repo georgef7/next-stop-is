cbc03b4c33d03e774798d98c8705c05b41d4b9ec
George Fang <57463760+georgef7@users.noreply.github.com>
2024-12-31 20:13:25 -0500

    Add theming

--- next-stop-is/src/App.tsx
+++ next-stop-is/src/App.tsx
@@ -1,42 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Button from '@mui/material/Button'
-import { Box } from '@mui/material'
+import { getTheme } from './AppStyles'
+import Box from '@mui/material/Box';
+import RadioGroup from '@mui/material/RadioGroup';
+import Radio from '@mui/material/Radio';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import { ThemeProvider } from '@mui/material/styles';
+import './App.css'
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    const [mode, setMode] = useState<'light' | 'dark'>('light'); // State to manage the theme mode
+    const theme = getTheme(mode);
+
+    if (count > 8) {
+        // reset
+        setCount(0);
+    }
+    // sample list of MARTA stations for now
+    const stationList = ['Doraville', 'Chamblee', 'Brookhaven', 'Lenox', 'Lindbergh Center', 'Arts Center', 'Midtown', 'North Avenue', 'Civic Center'];
 
     return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React: Next Stop is</h1>
-            <Box>A passenger information display project</Box>
-            <div className="card">
+        <ThemeProvider theme={theme}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    bgcolor: 'background.default',
+                    color: 'text.primary',
+                    borderRadius: 1,
+                    p: 3,
+                    minHeight: '56px'
+                }}
+            >
+                <FormControl>
+                    <FormLabel id="theme-toggle">Select a Theme</FormLabel>
+                    <RadioGroup
+                        aria-labelledby="theme-toggle"
+                        name="theme-toggle"
+                        row
+                        value={mode}
+                        onChange={(event) =>
+                            setMode(event.target.value as 'light' | 'dark')
+                        }
+                    >
+                        <FormControlLabel value="light" control={<Radio />} label="Light" />
+                        <FormControlLabel value="dark" control={<Radio />} label="Dark" />
+                    </RadioGroup>
+                </FormControl>
+            </Box>
+
+
+            <Box className="card"
+                sx={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    bgcolor: 'background.default',
+                    color: 'text.primary'
+                }}>
+                <h1>Next Stop is</h1>
                 <Button variant='contained' onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
+                    The next stop is: {stationList[count]}
                 </Button>
+                <h2>A passenger information display project</h2>
                 <Box data-testid='George-Message'>
                     Hello World! It's George! Meow~~
                 </Box>
-                <Box>
-                    {` /\_/\ \n
-( o.o ) \n
- > ^ <`}
-                </Box>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+            </Box>
+        </ThemeProvider>
     )
 }
 
